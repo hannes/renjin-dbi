5bc56c0a58278b59ce4330dd8e0b31c3aef444a8
Hannes Muehleisen <hannes@muehleisen.org>
2015-03-10 16:39:05 +0100

    credentials for orakel test

--- ROracle/src/test/R/oracle-test.R
+++ ROracle/src/test/R/oracle-test.R
@@ -1,4 +1,4 @@
 library(hamcrest)
 library(ROracle)
 
-test.driver <- function() renjinDBITest(dbConnect(ROracle(), url="jdbc:oracle:thin:@localhost", username="system", password="orakel"))
+test.driver <- function() renjinDBITest(dbConnect(ROracle(), url="jdbc:oracle:thin:@localhost", username="renjintest", password="renjintest"))
