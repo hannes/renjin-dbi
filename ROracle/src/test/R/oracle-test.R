1c5237305b3471eeb33b36f433edf8fa9334a594
Hannes Muehleisen <hannes@muehleisen.org>
2015-03-17 14:00:13 +0100

    mc ROracle

--- ROracle/src/test/R/oracle-test.R
+++ ROracle/src/test/R/oracle-test.R
@@ -1,4 +1,4 @@
 library(hamcrest)
 library(ROracle)
 
-test.driver <- function() renjinDBITest(dbConnect(ROracle(), url="jdbc:oracle:thin:@localhost", username="renjintest", password="renjintest"))
+test.driver <- function() renjinDBITest(dbConnect(ROracle(), url="jdbc:oracle:thin:@localhost/XE", username="renjintest", password="renjintest"))
