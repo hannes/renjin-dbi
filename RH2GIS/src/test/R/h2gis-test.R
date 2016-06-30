3f666644c986ae7ca752dd0c3d106e9286cb99ef
ebocher <ebocher@tregor>
2016-06-30 15:09:17 +0200

    Improve unit tests

--- RH2GIS/src/test/R/h2gis-test.R
+++ RH2GIS/src/test/R/h2gis-test.R
@@ -2,7 +2,8 @@ library(hamcrest)
 library(RH2GIS)
 
 test.driver <- function() {
-	con <- dbConnect(RH2GIS(), url="jdbc:h2:file:/tmp/db", username="sa", password="")
+        dbPath = paste ("jdbc:h2:file:",tempdir(), "/db", sep = "", collapse = NULL)
+	con <- dbConnect(RH2GIS(), url=dbPath, username="sa", password="")
 	print(loadSpatialFunctions(con));
         sq <- dbSendQuery(con,"DROP TABLE IF EXISTS VANNES; CREATE TABLE VANNES (the_geom geometry, id int)")
         stopifnot(identical(dbExistsTable(con,"VANNES"),TRUE))
