6d171ba5c92e163d7d430b7a3b399d03b0af79a9
ebocher <ebocher@tregor>
2016-06-30 12:33:47 +0200

    Add a method to spatialize an H2 database

--- RH2GIS/src/main/R/h2gis.R
+++ RH2GIS/src/main/R/h2gis.R
@@ -4,3 +4,14 @@ RH2GIS <- H2GIS <- function() {
 
 # load DBI to replicate GNU R 'Depends:'
 .onLoad <- function(libname, pkgname) library(DBI)
+
+
+loadSpatialFunctions <- function(con){
+    if(dbIsValid(con)){
+        dbSendQuery(con,"CREATE ALIAS IF NOT EXISTS H2GIS_EXTENSION FOR \"org.h2gis.ext.H2GISExtension.load\";")
+        dbSendQuery(con,"CALL H2GIS_EXTENSION()")
+        
+        return ("H2GIS functions have been successfully loaded.")
+    }
+        return ("The connection is not valid. Cannot load H2GIS functions")
+}
--- RH2GIS/src/test/R/h2gis-test.R
+++ RH2GIS/src/test/R/h2gis-test.R
@@ -3,8 +3,9 @@ library(RH2GIS)
 
 test.driver <- function() {
 	con <- dbConnect(RH2GIS(), url="jdbc:h2:file:/tmp/db", username="sa", password="")
-	sq <- dbSendQuery(con,"CREATE ALIAS IF NOT EXISTS H2GIS_EXTENSION FOR \"org.h2gis.ext.H2GISExtension.load\";")
-        sq <- dbSendQuery(con,"CALL H2GIS_EXTENSION()")
+	print(loadSpatialFunctions(con));
         sq <- dbSendQuery(con,"DROP TABLE IF EXISTS VANNES; CREATE TABLE VANNES (the_geom geometry, id int)")
         stopifnot(identical(dbExistsTable(con,"VANNES"),TRUE))
+        sq <- dbSendQuery(con,"INSERT INTO VANNES VALUES('POINT(0 1)'::GEOMETRY, 1)"); 
+        sq <- dbSendQuery(con,"SELECT ST_BUFFER(the_geom, 20) as the_geom FROM VANNES");
 }
